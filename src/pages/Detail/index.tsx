b19cee3913cb99feb8d0067e0a92aa8c472094ac
Roge Rodrigues <roge17@gmail.com>
2020-06-07 13:05:42 -0300

    Second Commit

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,8 +1,9 @@
 import React,{useEffect,useState} from 'react'
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
 import { useNavigation, useRoute } from '@react-navigation/native'
-import { View, Image, StyleSheet, TouchableOpacity, Text, SafeAreaView }  from 'react-native'
+import { View, Image, StyleSheet, TouchableOpacity, Text, SafeAreaView, Linking }  from 'react-native'
 import {RectButton} from 'react-native-gesture-handler'
+import * as MailComposer from 'expo-mail-composer';
 
 import api from '../../services/api'
 
@@ -44,6 +45,18 @@ const Details = () => {
     navigation.goBack()
   }
 
+
+  function handleWhatsapp() {
+    Linking.openURL(`whatsapp://send?´phone=${data.point.whatsapp}&text=Tenho interesse na coleta de resíduos`)
+  }
+
+  function handleComposeMail () {
+    MailComposer.composeAsync({
+      subject:'Interesse na coleta de resíduos',
+      recipients: [data.point.email],
+    })
+  }
+
   if(!data.point) {
     return null
   }
@@ -67,12 +80,12 @@ const Details = () => {
   </View>
   <View style={styles.footer}>
     
-    <RectButton style={styles.button} onPress={()=>{}}>
+    <RectButton style={styles.button} onPress={handleWhatsapp}>
       <FontAwesome name="whatsapp" size={20} color="#fff" />
       <Text style={styles.buttonText}>Whatsapp</Text>
     </RectButton>
 
-    <RectButton style={styles.button} onPress={()=>{}}>
+    <RectButton style={styles.button} onPress={handleComposeMail}>
       <Icon name="mail" size={20} color="#fff" />
       <Text style={styles.buttonText}>E-mail</Text>
     </RectButton>
@@ -89,7 +102,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 32,
-    paddingTop: 20,
+    paddingTop: 30,
   },
 
   pointImage: {
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,26 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 import { Feather as Icon } from '@expo/vector-icons'
-import { View,ImageBackground, Text, Image, StyleSheet } from 'react-native'
+import { View,ImageBackground, Text, Image, StyleSheet, TextInput,KeyboardAvoidingView,Platform } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native'
 
 
 const Home = () => {
-
+  const [uf,setUf] = useState('')
+  const [city,setCity] = useState('')
   const navigation = useNavigation()
 
   function handleNavigateToPoints() {
-    navigation.navigate('Points')
+    navigation.navigate('Points',{
+      uf,
+      city
+    })
+   
   }
 
   return (
+  <KeyboardAvoidingView style={{flex:1}} behavior={Platform.OS ==='ios' ? 'padding' : undefined} >
   <ImageBackground 
   source={require('../../assets/home-background.png')} 
   style={styles.container}
@@ -27,6 +33,26 @@ const Home = () => {
     </View>
 
     <View style={styles.footer}>
+
+    <TextInput
+    style={styles.input}
+    placeholder="Digite a UF"
+    value={uf}
+    maxLength={2}
+    autoCapitalize="characters"
+    autoCorrect={false}
+    onChangeText={setUf}
+    />
+    <TextInput
+    style={styles.input}
+    placeholder="Digite a Cidade"
+    value={city}
+    onChangeText={setCity}
+    autoCorrect={false}
+
+    />
+    
+
       <RectButton style={styles.button} onPress={handleNavigateToPoints}>
         <View style={styles.buttonIcon}>
          <Icon name="arrow-right"color="#fff" size={24} />
@@ -36,7 +62,9 @@ const Home = () => {
         </Text>
       </RectButton>
     </View>
-  </ImageBackground>)
+  </ImageBackground>
+  </KeyboardAvoidingView>
+  )
 }
 
 
